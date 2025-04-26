b88f2456047c603842b3da7a908cafb25d00359a
alexfloripavieira <alexsandervieira1974@gmail.com>
2025-04-26 14:14:52 -0300

    feat:added aptos template

--- app/urls.py
+++ app/urls.py
@@ -19,6 +19,9 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from aptos import views
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("aptos/", views.lista_aptos, name="lista_aptos"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- aptos/views.py
+++ aptos/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Aptos
+
+
+def lista_aptos(request):
+    aptos = Aptos.objects.select_related("building_name").prefetch_related("fotos")
+    return render(request, "aptos_lista.html", {"aptos": aptos})
