54bb1818d2a761b284c2dcc25df9d4e1d26577d5
alexfloripavieira <alexsandervieira1974@gmail.com>
2025-04-27 15:23:29 -0300

    feat:added builders page

--- aptos/urls.py
+++ aptos/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.lista_aptos, name="lista_aptos"),
+    path("aptos/", views.lista_aptos, name="lista_aptos"),
+    path("builders/", views.listar_builders, name="listar_builders"),
 ]
--- aptos/views.py
+++ aptos/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
-from .models import Aptos
+from .models import Aptos, Builders
 
 
 def lista_aptos(request):
     aptos = Aptos.objects.select_related("building_name").prefetch_related("fotos")
     return render(request, "aptos/aptos_lista.html", {"aptos": aptos})
+
+
+def listar_builders(request):
+    builders = Builders.objects.prefetch_related("builder_fotos").all()
+    return render(request, "aptos/builders_lista.html", {"builders": builders})
