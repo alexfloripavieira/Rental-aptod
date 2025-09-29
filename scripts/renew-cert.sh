e2248c2bb64483096fc65c449b80d232eb4dd1b5
alexfloripavieira <alexsander.vieira.itlean@edglobo.com.br>
2025-09-29 17:24:12 -0300

    fix

--- scripts/renew-cert.sh
+++ scripts/renew-cert.sh
@@ -6,7 +6,12 @@ PROJECT_DIR="/home/ubuntu/Rental-aptod"
 NGINX_SSL_DIR="$PROJECT_DIR/nginx/ssl"
 COMPOSE_CMD="docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile production"
 
-certbot renew --quiet \
+DRY_RUN_FLAG=""
+if [[ "${1:-}" == "--dry-run" ]]; then
+  DRY_RUN_FLAG="--dry-run"
+fi
+
+certbot renew $DRY_RUN_FLAG --quiet \
   --pre-hook "$COMPOSE_CMD stop nginx" \
   --deploy-hook "cp /etc/letsencrypt/live/$CERT_DOMAIN/fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem && cp /etc/letsencrypt/live/$CERT_DOMAIN/privkey.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem && chown ubuntu:ubuntu $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem" \
   --post-hook "$COMPOSE_CMD up -d nginx"
