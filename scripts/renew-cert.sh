8e22c55d23c4854dd3a6d0ad688facb30d022917
alexfloripavieira <alexsander.vieira.itlean@edglobo.com.br>
2025-09-29 17:15:18 -0300

    fixes

--- scripts/renew-cert.sh
+++ scripts/renew-cert.sh
@@ -6,9 +6,7 @@ PROJECT_DIR="/home/ubuntu/Rental-aptod"
 NGINX_SSL_DIR="$PROJECT_DIR/nginx/ssl"
 COMPOSE_CMD="docker compose -f docker-compose.yml -f docker-compose.prod.yml --profile production"
 
-certbot renew --quiet --deploy-hook "\
-  cp /etc/letsencrypt/live/$CERT_DOMAIN/fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem && \
-  cp /etc/letsencrypt/live/$CERT_DOMAIN/privkey.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem && \
-  chown ubuntu:ubuntu $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem && \
-  cd $PROJECT_DIR && \
-  $COMPOSE_CMD exec -T nginx nginx -s reload"
+certbot renew --quiet \
+  --pre-hook "$COMPOSE_CMD stop nginx" \
+  --deploy-hook "cp /etc/letsencrypt/live/$CERT_DOMAIN/fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem && cp /etc/letsencrypt/live/$CERT_DOMAIN/privkey.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem && chown ubuntu:ubuntu $NGINX_SSL_DIR/$CERT_DOMAIN.fullchain.pem $NGINX_SSL_DIR/$CERT_DOMAIN.privkey.pem" \
+  --post-hook "$COMPOSE_CMD up -d nginx"
